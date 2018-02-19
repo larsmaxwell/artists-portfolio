dd22377b116e1d9537893728ed40bb007bc65aeb
Lauren <mlmxwell@gmail.com>
2018-02-18 18:19:08 -0600

    Adds additional dependencies for heroku deployment.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 //Install express server
 const express = require('express');
+const http = require('http');
+const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.set(port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Running...'));
