028b44b416dcf1cf705420f11f0ed12b118fb0ca
Lauren <mlmxwell@gmail.com>
2020-03-26 12:12:43 -0700

    Adds video support and updates gallery view.

--- src/app/components/art-work-view/art-work-view.component.ts
+++ src/app/components/art-work-view/art-work-view.component.ts
@@ -3,11 +3,10 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Subscription } from 'rxjs/Subscription';
 
 import * as blocksToHtml from '@sanity/block-content-to-html';
-
 
 // App Specific
 import { ArtWork } from '../../types/art-work';
@@ -22,13 +21,14 @@ export class ArtWorkViewComponent implements OnInit {
 
   work: ArtWork;
   albumId2: string;
+  safeURL: SafeResourceUrl;
   descriptionHtmlBlock: String; 
 
   constructor(
     private route: ActivatedRoute,
     private artWorkService: ArtWorkService,
     private location: Location,
-    private sanitizer: DomSanitizer,
+    private _sanitizer: DomSanitizer
   ) {
   }
 
@@ -52,6 +52,8 @@ export class ArtWorkViewComponent implements OnInit {
     this.artWorkService.getWorkByPermalink(client, permalink).then(
       data => {
         this.work = data[0];
+        this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl(data[0].mediaUrl);
+
         if (data[0].album) {
           this.albumId2 = data[0].album._ref;
         }
--- src/app/types/art-work.ts
+++ src/app/types/art-work.ts
@@ -7,6 +7,7 @@ export class ArtWork {
     category: { _ref: string, _type: string };
     description: Array<{_key: string, _type: string, children: Array<{marks:  Array<{}>, text: string }>, markDefs: Array<{_key: string, _type: string, href: string, style: string}>}>;
     name: string;
+    mediaUrl: string;
     galleryview: boolean;
     slug: {_type: string, curent: string};
 }
