39692638ce617751eca5d24f7d98ca973a0fcc3c
Lauren <mlmxwell@gmail.com>
2021-03-21 22:12:50 -0700

    Start refactor on Slides. Still need to fix pagination bug.

--- src/app/components/img-zoom/img-zoom.component.ts
+++ src/app/components/img-zoom/img-zoom.component.ts
@@ -9,7 +9,7 @@ import { SanityService } from '../../services/sanity.service';
 import { WindowRefService } from '../../services/window-ref.service';
 
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faArrowLeft, faArrowRight, faChevronCircleLeft, faChevronCircleRight} from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faBullseye, faChevronCircleLeft, faChevronCircleRight} from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -25,12 +25,14 @@ export class ImgZoomComponent implements OnInit {
   images: any;
   currentImg: any;
   imgControls: any;
+  paginationCtrl: any;
   imgIndex: number;
+  maxPagination: number;
   desHeight: string;
-  desWidth: string;
+  homePage: boolean;
+  activePaginationItems: [];
   sanityInstance: any;
   sanityImgBuilder: any;
-  slideshow: boolean;
   currPermalink: string;
   isBrowser: boolean;
   getAlbumId: string;
@@ -53,40 +55,57 @@ export class ImgZoomComponent implements OnInit {
   ) {
     library.addIcons(this.faArrowLeft, this.faArrowRight, this.faChevronCircleLeft);
     this.isBrowser = isPlatformBrowser(platformId);
+
+    this.getSanity();
+    this.getSanityUrlBuilder();
+
+    this.maxPagination = 6;
   }
 
   ngOnInit() {
-    this.getSanity();
-    this.getSanityUrlBuilder();
-    const currentRoute = this.route ? this.route : this.route;
-    const snapshotCapture = currentRoute ? currentRoute.snapshot : currentRoute.snapshot;
+    // Set global variables based on whats available in the root
+    this.setRouteParameterGlobalValues();
 
-    // Get information about the album
-    this.getAlbumId = this.albumId || this.route.snapshot.paramMap.get('albumId');
-    this.getAlbum(this.getAlbumId);
-
-    this.imgIndex = parseInt(this.route.snapshot.paramMap.get('imgId')) || 0;
-    this.currPermalink = this.route.snapshot.paramMap.get('permalink');
-
-    if (this.imgIndex && this.currPermalink) {
-      this.slideshow = true;
-    }
-
-    this.getAlbumImages(this.getAlbumId);
+    this.getAlbumImages(this.getAlbumId); // Only need to do this once since images are loaded on load
 
     this.route.params.subscribe(params => {
-      this.getAlbumId = params.albumId ? params.albumId : this.albumId;
-      this.imgIndex = params.imgId ? parseInt(params.imgId) : 0;
+      this.setRouteParameterGlobalValues();
+
       this.getAlbumImages(this.getAlbumId);
     });
-
   }
+
   ngOnChanges(changes: SimpleChanges) {
-    
-    this.getAlbumId = this.route.snapshot.paramMap.get('albumId') ? this.route.snapshot.paramMap.get('albumId') : this.albumId;
-    this.imgIndex = this.route.snapshot.paramMap.get('imgId') ? parseInt(this.route.snapshot.paramMap.get('imgId')) : 0;
+    this.setRouteParameterGlobalValues();
 
     this.getAlbumImages(this.getAlbumId);
+  }
+
+  setRouteParameterGlobalValues() {
+    // Homepage route params
+    this.homePage = !this.route.firstChild; // opposite of child page
+
+    // Get information about the album and Img ID on the page
+    if (this.route.firstChild && this.route.firstChild.snapshot.paramMap.get('albumId')) {
+      this.getAlbumId = this.route.firstChild.snapshot.paramMap.get('albumId');
+    } 
+    else if (this.route.snapshot.paramMap.get('albumId')) {
+      this.getAlbumId = this.route.snapshot.paramMap.get('albumId');
+    }
+    else {
+      this.getAlbumId = this.albumId; // @input albumID
+    }
+
+    if (this.route.firstChild && this.route.firstChild.snapshot.paramMap.get('imgId')) {
+      this.imgIndex = parseInt(this.route.firstChild.snapshot.paramMap.get('imgId'));
+    } 
+    else if (this.route.snapshot.paramMap.get('imgId')) {
+      this.imgIndex = parseInt(this.route.snapshot.paramMap.get('imgId'))
+    }
+    else {
+      this.imgIndex = this.imgIndex | 0; // @input albumID
+    }
+    this.currPermalink = this.route.snapshot.paramMap.get('permalink');
   }
 
   getWindowHeight() {
@@ -94,36 +113,102 @@ export class ImgZoomComponent implements OnInit {
       return window.innerHeight;
     }
   }
+  // General function for Updating most portions of the view when
+  // the route changes
+  updateSlideshowView() {
+    this.setActivePaginationItems();
+
+    this.updateImageControls();
+  }
+
+  goToSlide(imgIndex) {
+    // Navigate the router to a new location
+    if (this.homePage === false) {
+      this.router.navigate([this.albumId, imgIndex], { relativeTo: this.route });
+    }
+    this.imgIndex = imgIndex;
+    
+    this.updateSlideshowView();
+  }
+
+  updateIndexSlideAndRoute() {
+    if (this.images) {
+      if (this.imgIndex <= this.images.length-1) {
+        this.currentImg = this.images[this.imgIndex];
+
+        this.goToSlide(this.imgIndex);
+      } else { // If there's a link to an index that doesn't exist.
+        
+        this.imgIndex = 0;
+        this.currentImg = this.images[0];
+
+        this.goToSlide(0); // go to slide and update slideshow view
+      }
+    }
+  }
+
+  setActivePaginationItems() {
+    // Get the number of Sets -- how many times can the max amt in a set 
+    let numberSets = Math.ceil(this.images.length / this.maxPagination); // number of pagination menus to loop through + skip ahead
+    let maxItem = (numberSets * this.maxPagination) - this.maxPagination;
+
+    // Img index
+    let imgIndex = this.imgIndex;
+    let pagDevIndex = imgIndex / (this.maxPagination);
+    let pagDevIndexFlr = Math.floor(pagDevIndex);
+    let paginationStartIndex = pagDevIndexFlr * (this.maxPagination);
+
+    // Not sure what I was trying to do here: 
+    // let nextDisabledRemainder = Math.ceil(this.images.length % this.maxPagination);
+    // let nextDisabledRemainderCleaned = nextDisabledRemainder === 0 
+    //     ? this.maxPagination : nextDisabledRemainder;
+    // let noRemainderGhost = nextDisabledRemainderCleaned * this.maxPagination; // Next "Perfect" match
+    // let cutOffItem = noRemainderGhost - this.maxPagination
+
+    this.paginationCtrl = {
+      paginationStartIndex: paginationStartIndex,
+      isPrevDisabled: paginationStartIndex <= 0,
+      isNextDisabled: paginationStartIndex >= maxItem,
+      activePaginationItems: this.images.filter((image, index) => {
+        console.log(paginationStartIndex);
+        return index >= paginationStartIndex && index <= paginationStartIndex + (this.maxPagination-1);
+      })
+    }
+  }
+
+  getProperSlideHeight(img) {
+    const localimg = img;
+  }
 
   getAlbumImages(id) {
+
+    const imgIDCopy =  this.imgIndex;
+
     this.ArtWorkAlbumService.getAlbumImages(this.sanityInstance, id).subscribe(data => {
       // console.log(this.images);
       this.images = data;
       let albumLength = this.images.length;
 
-      this.updateCurrentImage(data[this.imgIndex]);
+      this.updateIndexSlideAndRoute();
 
-      this.updateImageControls();
     });
   }
 
-  updateCurrentImage(image) {
-    this.currentImg = image;
-  }
-
   updateImageControls() {
-    let desLength = this.images.length - 1;
-    let imgInd = this.imgIndex
-    let next = imgInd === desLength ? 0 : imgInd + 1;
-    let prev = imgInd === 0 ? desLength : imgInd - 1;
-
-    this.imgControls = {
-        currentImg: this.currentImg,
-        currentIndex: imgInd,
-        nextId: next,
-        prevId: prev,
-        permalink: this.currPermalink
-    };
+    if (this.images) {
+      let desLength = this.images.length - 1;
+      let imgInd = this.imgIndex;
+      let next = imgInd === desLength ? 0 : imgInd + 1;
+      let prev = imgInd === 0 ? desLength : imgInd - 1;
+  
+      this.imgControls = {
+          currentImg: this.currentImg,
+          currentIndex: imgInd,
+          nextId: next,
+          prevId: prev,
+          permalink: this.currPermalink
+      };
+    }
   }
 
   updateRouter(forward:boolean) {
@@ -137,24 +222,13 @@ export class ImgZoomComponent implements OnInit {
     }
 
     // Set the asset for the index
-    this.updateCurrentImage(this.images[this.imgIndex].asset);
+    this.currentImg = this.images[this.imgIndex];
     // Update the controls available to the view
     this.updateImageControls();
   }
 
   isCorrectIndex(id) {
     return parseInt(id) === this.imgIndex;
-  }
-
-  getAlbum(id: string) {
-
-    // const currPermalink = this.route.firstChild.snapshot.paramMap.get('permalink');
-
-    this.ArtWorkAlbumService.getAlbumById(id)
-      .subscribe(album => {
-        this.album = album;
-        console.log(album);
-      });
   }
 
   getSanity() {
@@ -169,19 +243,9 @@ export class ImgZoomComponent implements OnInit {
     return this.sanityImgBuilder.image(source)
   }
 
-  getNextIndex(index: number) {
-    if (index === this.album.images.length - 1) {
-      return 0;
-    }
-    else {
-      return index+1;
-    }
-  }
-
   isMobileSize() {
     if (this.isBrowser) {
       return window.innerWidth <= 768;
     }
   }
-
 }
