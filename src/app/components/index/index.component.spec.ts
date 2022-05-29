0c92560ee77f1ad1bb7b035d953b45bd1eec2a6f
Lauren <mlmxwell@gmail.com>
2022-05-29 14:18:01 -0700

    Fixes tests.

--- src/app/components/index/index.component.spec.ts
+++ src/app/components/index/index.component.spec.ts
@@ -1,12 +1,12 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Meta, Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { of } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { IndexComponent } from './index.component';
 
 const siteDatas = require('../../../test-data/siteInfo.json');
-
 
 describe('IndexComponent', () => {
   let component: IndexComponent;
@@ -15,22 +15,19 @@ describe('IndexComponent', () => {
     mockTitle,
     mockMeta;
   const SITEDATA: any = siteDatas[0];
-
+  mockMeta = {
+    updateTag: () => of(false)
+  }
+  mockTitle = {
+    setTitle: () => of(false)
+  }
   beforeEach(() => {
     mockActivatedRoute = {
-      data: {
+      data: new BehaviorSubject({
         siteInfo: SITEDATA,
         home: false,
-        subscribe: () => {
-          return of({
-            siteInfo: SITEDATA,
-            home: false,
-          })
-        }
-      },
+        })
     };
-    mockMeta = jasmine.createSpyObj(['updateTag']);
-    mockTitle = jasmine.createSpyObj(['setTitle']);
 
     TestBed.configureTestingModule({
       declarations: [ IndexComponent ],
@@ -42,12 +39,14 @@ describe('IndexComponent', () => {
         {provide: Meta, useValue: mockMeta},
         {provide: Title, useValue: mockTitle}
       ]
-    });
-    // .compileComponents();
+    })
+    .compileComponents();
+
+    spyOn(mockMeta, 'updateTag').and.callThrough();
+    spyOn(mockTitle, 'setTitle').and.callThrough();
 
     fixture = TestBed.createComponent(IndexComponent);
     component = fixture.componentInstance;
-
     component.footerNav = SITEDATA.footerNav;
   });
 
@@ -61,7 +60,7 @@ describe('IndexComponent', () => {
     expect(mockMeta.updateTag).toHaveBeenCalled();
   });
 
-  it('should update meta tags', () => {
+  it('should update meta title tag', () => {
     fixture.detectChanges();
     expect(mockTitle.setTitle).toHaveBeenCalled();
   });
